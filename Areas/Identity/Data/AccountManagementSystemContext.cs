44fd8b11e6a69e957c1e4781094cfc89d65b6168
Shawon Kumar Modak <89266007+shawonkumar01@users.noreply.github.com>
2025-06-26 12:57:36 +0600

    identity add

---
+++ Areas/Identity/Data/AccountManagementSystemContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AccountManagementSystem.Data;
+
+public class AccountManagementSystemContext : IdentityDbContext<IdentityUser>
+{
+    public AccountManagementSystemContext(DbContextOptions<AccountManagementSystemContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,12 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using AccountManagementSystem.Data;
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("AccountManagementSystemContextConnection") ?? throw new InvalidOperationException("Connection string 'AccountManagementSystemContextConnection' not found.");;
+
+builder.Services.AddDbContext<AccountManagementSystemContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<AccountManagementSystemContext>();
 
 // Add services to the container.
 builder.Services.AddRazorPages();
