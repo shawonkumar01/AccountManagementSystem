d335fcd4a5bc8d250225e01ba7b36f03114b580e
Shawon Kumar Modak <89266007+shawonkumar01@users.noreply.github.com>
2025-06-28 14:42:26 +0600

    Voucher Entry Module Completed for Viewer

--- Pages/Dashboard.cshtml.cs
+++ Pages/Dashboard.cshtml.cs
@@ -1,30 +1,44 @@
 ﻿using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 using AccountManagementSystem.Data;
+using AccountManagementSystem.Models;
+using System.Collections.Generic;
 using System.Linq;
 
 [Authorize(Roles = "Admin,Accountant,Viewer")]
 public class DashboardModel : PageModel
 {
-    private readonly ChartOfAccountsRepository _repo;
+    private readonly ChartOfAccountsRepository _accountRepo;
+    private readonly VoucherRepository _voucherRepo;
 
-    public DashboardModel(ChartOfAccountsRepository repo)
+    public DashboardModel(ChartOfAccountsRepository accountRepo, VoucherRepository voucherRepo)
     {
-        _repo = repo;
+        _accountRepo = accountRepo;
+        _voucherRepo = voucherRepo;
     }
 
     [BindProperty(SupportsGet = true)]
     public string SearchCode { get; set; }
 
+    [BindProperty(SupportsGet = true)]
+    public string ReferenceSearch { get; set; }
+
     public ChartOfAccount SearchedAccount { get; set; }
+    public VoucherWithDetails SearchedVoucher { get; set; }
 
     public void OnGet()
     {
         if (!string.IsNullOrWhiteSpace(SearchCode))
         {
-            var allAccounts = _repo.GetAll();
-            SearchedAccount = allAccounts.FirstOrDefault(a => a.AccountCode == SearchCode);
+            SearchedAccount = _accountRepo.GetAll()
+                .FirstOrDefault(a => a.AccountCode == SearchCode);
+        }
+
+        if (!string.IsNullOrWhiteSpace(ReferenceSearch))
+        {
+            SearchedVoucher = _voucherRepo.GetAllVouchersWithDetails()
+                .FirstOrDefault(v => v.ReferenceNo == ReferenceSearch);
         }
     }
 }
